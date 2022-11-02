619d6bd0c620cff7c541087b6c37990353b8cb9b
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-02 15:52:11 -0700

    Adds update form to ShoesShow component with onSubmit method

--- src/ShoesNew.jsx
+++ src/ShoesNew.jsx
@@ -1,12 +1,47 @@
+import axios from "axios";
+
 export function ShoesNew() {
+  const handleCreateShoe = (params) => {
+    console.log(params);
+    axios.post("http://localhost:3000/shoes.json", params).then((response) => {
+      const newShoe = response.data;
+      console.log(newShoe);
+      window.location.href = "/";
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    handleCreateShoe(params);
+    event.target.reset();
+  };
+
   return (
-    <form id="shoes-new">
-      <input type="text" name="name"></input>
-      <input type="text" name="color"></input>
-      <input type="text" name="description"></input>
-      <input type="text" name="style"></input>
-      <input type="text" name="image"></input>
-      <input type="submit"></input>
+    <form id="shoes-new" onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input type="text" name="name"></input>
+      </div>
+      <div>
+        <label>Color</label>
+        <input type="text" name="color"></input>
+      </div>
+      <div>
+        <label>Description</label>
+        <input type="text" name="description"></input>
+      </div>
+      <div>
+        <label>Style</label>
+        <input type="text" name="style"></input>
+      </div>
+      <div>
+        <label>Image</label>
+        <input type="text" name="image"></input>
+      </div>
+      <div>
+        <input type="submit"></input>
+      </div>
     </form>
   );
 }
--- src/ShoesShow.jsx
+++ src/ShoesShow.jsx
@@ -1,11 +1,45 @@
 export function ShoesShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    event.target.reset();
+  };
+
   return (
     <div>
-      <img src={props.shoe.image} alt={props.shoe.description} />
-      <h2>{props.shoe.name}</h2>
-      <p>{props.shoe.color}</p>
-      <p>{props.shoe.description}</p>
-      <p>{props.shoe.style}</p>
+      <div>
+        <img src={props.shoe.image} alt={props.shoe.description} />
+        <h2>{props.shoe.name}</h2>
+        <p>{props.shoe.color}</p>
+        <p>{props.shoe.description}</p>
+        <p>{props.shoe.style}</p>
+      </div>
+
+      <form id="shoes-new" onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <input type="text" name="name" defaultValue={props.shoe.name}></input>
+        </div>
+        <div>
+          <label>Color</label>
+          <input type="text" name="color" defaultValue={props.shoe.color}></input>
+        </div>
+        <div>
+          <label>Description</label>
+          <input type="text" name="description" defaultValue={props.shoe.description}></input>
+        </div>
+        <div>
+          <label>Style</label>
+          <input type="text" name="style" defaultValue={props.shoe.style}></input>
+        </div>
+        <div>
+          <label>Image</label>
+          <input type="text" name="image" defaultValue={props.shoe.image}></input>
+        </div>
+        <div>
+          <input type="submit"></input>
+        </div>
+      </form>
     </div>
   );
 }

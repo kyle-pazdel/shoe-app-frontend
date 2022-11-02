adbec7afb1fa283f5904b349db8aa6b268d33c77
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-02 08:26:49 -0700

    Adds modal show/hide functioinality from props in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Modal } from "./Modal";
 import { ShoesIndex } from "./ShoesIndex";
+import { ShoesShow } from "./ShoesShow";
 
 export function Home() {
   const [shoes, setShoes] = useState([]);
+  const [isShoeVisible, setIsShoeVisible] = useState(false);
+  const [currentShoe, setCurrentShoe] = useState({});
 
   const handleIndexShoes = () => {
     axios.get("http://localhost:3000/shoes.json").then((response) => {
@@ -12,11 +16,23 @@ export function Home() {
     });
   };
 
+  const handleShowShoe = (shoe) => {
+    setIsShoeVisible(true);
+    setCurrentShoe(shoe);
+  };
+
+  const handleHideShoe = () => {
+    setIsShoeVisible(false);
+  };
+
   useEffect(handleIndexShoes, []);
 
   return (
     <div>
-      <ShoesIndex shoes={shoes} />
+      <ShoesIndex shoes={shoes} onSelectShoe={handleShowShoe} />
+      <Modal show={isShoeVisible} onClose={handleHideShoe}>
+        <ShoesShow shoe={currentShoe} />
+      </Modal>
     </div>
   );
 }
---
+++ src/ShoesShow.jsx
@@ -0,0 +1,7 @@
+export function ShoesShow(props) {
+  return (
+    <div>
+      <h1>A Shoe!</h1>
+    </div>
+  );
+}
